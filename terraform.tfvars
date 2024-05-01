029de81bb3f94209ed6dc12d26a49e2c73873426
Filip3Kx <filip.kubawski@gmail.com>
2024-05-01 22:58:23 +0000

    format

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,24 +1,24 @@
 terraform_service_account = "terraform@linen-hook-422021-a8.iam.gserviceaccount.com"
-not_impersonate = false
+not_impersonate           = false
 gcp_info = {
-    project = "linen-hook-422021-a8"
-    region = "europe-west1"
-    zone = "europe-west1-b"
+  project = "linen-hook-422021-a8"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
 }
 
 instance_info = {
-    machine_type = "n1-standard-1"
-    image_family = "debian-11"
-    image_project = "debian-cloud"
+  machine_type  = "n1-standard-1"
+  image_family  = "debian-11"
+  image_project = "debian-cloud"
 }
 
 instance_tags = ["allow-ssh", "allow-http", "terraform-tp"]
 
 autoscaler_info = {
-    max_replicas = 5
-    min_replicas = 2
-    cooldown_period = 60
-    cpu_utilization_target = 0.5
+  max_replicas           = 5
+  min_replicas           = 2
+  cooldown_period        = 60
+  cpu_utilization_target = 0.5
 }
 
 lb_tags = ["terraform-tp"]
--- variables.tf
+++ variables.tf
@@ -3,47 +3,47 @@ variable "terraform_service_account" {
 }
 
 variable "not_impersonate" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "gcp_info" {
-  type = map
+  type = map(any)
   default = {
     project = "linen-hook-422021-a8"
-    region = "europe-west1"
-    zone = "europe-west1-b"
+    region  = "europe-west1"
+    zone    = "europe-west1-b"
 
   }
 }
 variable "instance_info" {
-  type = map
+  type = map(any)
   default = {
-    machine_type = "n1-standard-1"
-    image_family = "debian-11"
+    machine_type  = "n1-standard-1"
+    image_family  = "debian-11"
     image_project = "debian-cloud"
   }
 }
 variable "instance_tags" {
-  type = list(string)
+  type    = list(string)
   default = ["allow-ssh", "allow-http", "terraform-tp"]
-  
+
 }
 variable "autoscaler_info" {
-  type = map
+  type = map(any)
   default = {
-    max_replicas = 5
-    min_replicas = 2
-    cooldown_period = 60
+    max_replicas           = 5
+    min_replicas           = 2
+    cooldown_period        = 60
     cpu_utilization_target = 0.5
-  }  
+  }
 }
 
 variable "lb_tags" {
-  type = list(string)
+  type    = list(string)
   default = ["terraform-tp"]
 }
 variable "lb_port" {
-    type = number
-    default = 80
+  type    = number
+  default = 80
 }
